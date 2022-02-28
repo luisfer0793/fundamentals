398ae8d70e7d9904a84ff2a3cee447e60464fad5
luisfer0793 <luisfer.0793@gmail.com>
2022-02-28 00:04:49 -0600

    feature(reduce, filter): Filter and reduce methods Add filter and reduce methods to show how it works.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,9 +6,21 @@ fun greet(name: String, message: String): String {
 	return "Hi, $name. $message"
 }
 
+const val OS = "Windows"
+const val name = "Fernando"
+const val message = "This is my first kotlin message"
+const val age = 28
+const val initialLetter = 'F'
+val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
 fun main() {
-	val name = "Fernando"
-	val message = "This is my first kotlin message"
+	val names = arrayOf("Nidia", "Gaby", "Brenda", "Luis")
 	println(greet(name, message))
 	println("The result of sum 10 + 10 is: ${add(10, 10)}")
+	println("The initial letter of $name is $initialLetter")
+	println("I'm $age years old")
+	val filteredNames = names.filter { name -> name.contains("a") }
+	val sumOfEvenNumbers = numbers.filter { number -> number % 2 != 0 }.sum()
+	println(filteredNames)
+	println(sumOfEvenNumbers)
 }
